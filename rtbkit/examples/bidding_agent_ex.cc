b9c6055f673ab7b94f517e1b9bb78656e4e155c6
Daniel Podolsky <onokonem@gmail.com>
2015-06-15 00:38:18 +0300

    some more debug logging

--- rtbkit/examples/bidding_agent_ex.cc
+++ rtbkit/examples/bidding_agent_ex.cc
@@ -91,6 +91,11 @@ struct FixedPriceBiddingAgent :
     {
         config = AgentConfig();
 
+        // To make this bidding_agent_ex Smaato compatible
+        Json::Value smaatoMap;
+        smaatoMap["seat"] = "123";
+        config.providerConfig["smaato"] = smaatoMap;
+
         // Accounts are used to control the allocation of spending budgets for
         // an agent. The whole mechanism is fully generic and can be setup in
         // whatever you feel it bests suits you.
@@ -128,11 +133,6 @@ struct FixedPriceBiddingAgent :
         // Configures the agent to only receive 10% of the bid request traffic
         // that matches its filters.
         config.bidProbability = 0.1;
-
-        // To make this bidding_agent_ex Smaato compatible
-        Json::Value smaatoMap;
-        smaatoMap["seat"] = "123";
-        config.providerConfig["smaato"] = smaatoMap;
 
         // Tell the world about our config. We can change the configuration of
         // an agent at any time by calling this function.
