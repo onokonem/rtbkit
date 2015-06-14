4278cd1f9c7ccc259d1d8fed8c36bb45e4fa5d01
Daniel Podolsky <onokonem@gmail.com>
2015-06-15 02:38:23 +0300

    some more debug logging

--- rtbkit/core/agent_configuration/agent_config.cc
+++ rtbkit/core/agent_configuration/agent_config.cc
@@ -35,7 +35,7 @@ Creative(int width, int height, std::string name, int id, const std::string deal
     Json::Value mimeTypes;
     mimeTypes[0] = "image/gif";
     Json::Value smaatoMap;
-    smaatoMap["adm"] = "smaato.adm";
+    smaatoMap["adm"] = "<ad xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"smaato_ad_v0.9.xsd\" modelVersion=\"0.9\"><imageAd><clickUrl>http://mysite.com/landingpages/mypage/</clickUrl><imgUrl>http://mysite.com/images/myad.jpg</imgUrl><width>728</width><height>90</height><toolTip>This is a tooltip text</toolTip><additionalText>Additional text to be displayed</additionalText><beacons><beacon>http://mysite.com/beacons/mybeacon1</beacon><beacon>http://mysite.com/beacons/mybeacon2</beacon></beacons></imageAd></ad>";
     smaatoMap["nurl"] = "smaato.nurl";
     smaatoMap["adid"] = "smaato.adid";
     smaatoMap["adomain"] = adomain;
